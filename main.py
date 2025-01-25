3988cabf91f4b00b517ec4ffd58d69e1624fade1
Houssamkarroum <karroumhoussam2020@gmail.com>
2025-01-25 17:45:49 +0100

    creating docker-compose file for all needed services and also a generate data function in main.py

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+import random
+import time
+from datetime import datetime
+from faker import Faker
+from confluent_kafka import SerializingProducer
+fake = Faker()
+def generate_sales_transaction():
+    user = fake.single_profile()
+    return {
+        "transactionId": fake.uuid4(),
+        "productId": random.choice(['product1', 'product2', 'product3', 'product4', 'product5', 'product6']),
+        "productName": fake.choice(['laptop', 'mobile', 'tablet', 'watch', 'headpho','speaker']),
+        "productCategory": fake.choice(['electronics', 'fashion', 'grocery', 'home', 'beauty', 'sports']),
+        "productPrice": round(random.uniform(100, 1000), 2),
+        "productQuantity": random.randint(1, 10),
+        "productBrand": random.choice(['apple', 'samsung', 'sony', 'mi', 'oneplus', 'boat']),
+        "currency": random.choice(['USD', 'MAD']),
+        "customerId": user['username'],
+        "transactionDate": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "paymentMethod": fake.choice(['credit_card', 'debit_card', 'online_payment'])
+    }
+def delivery_report(err, msg):
+    if err is not None:
+        print("Delivery failed for User record {}: {}".format(msg.key(), err))
+        return
+    print('User record {} successfully produced to {} [{}] at offset {}'.format(
+        msg.key(), msg.topic(), msg.partition(), msg.offset()))
+def main():
+    topic = 'sales'
+    producer = SerializingProducer({
+        'bootstrap.servers': 'localhost:9092'
+    })
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
